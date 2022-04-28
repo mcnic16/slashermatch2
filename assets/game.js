8a3361c1c1b6b8b39dc03cbca7f97580d504130d
mcnic16 <jamesmcnic@yahoo.co.uk>
2022-04-28 16:14:03 +0000

    pictures moved html to javascript

--- assets/game.js
+++ assets/game.js
@@ -6,3 +6,23 @@ const same = 0;
 
 paired.textContent = same;
 
+// game pictures
+const gameData = () => [
+    {imgsrc: "assets/images/1.jpg", name: "1"},
+    {imgsrc: "assets/images/2.jpg", name: "2"},
+    {imgsrc: "assets/images/chucky.jpg", name: "chucky"},
+    {imgsrc: "assets/images/freddie.jpg", name: "freddie"},
+    {imgsrc: "assets/images/jason.jpg", name: "jason"},
+    {imgsrc: "assets/images/jigsaw.jpg", name: "jigsaw"},
+    {imgsrc: "assets/images/pennywise.jpg", name: "1pennywise"},
+    {imgsrc: "assets/images/scream.jpg", name: "scream"},
+    {imgsrc: "assets/images/1.jpg", name: "1"},
+    {imgsrc: "assets/images/2.jpg", name: "2"},
+    {imgsrc: "assets/images/chucky.jpg", name: "chucky"},
+    {imgsrc: "assets/images/freddie.jpg", name: "freddie"},
+    {imgsrc: "assets/images/jason.jpg", name: "jason"},
+    {imgsrc: "assets/images/jigsaw.jpg", name: "jigsaw"},
+    {imgsrc: "assets/images/pennywise.jpg", name: "1pennywise"},
+    {imgsrc: "assets/images/scream.jpg", name: "scream"},
+];
+ 
