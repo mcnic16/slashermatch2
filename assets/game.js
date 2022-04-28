26c799962832b8c08ac570e49e6527ea507c280a
mcnic16 <jamesmcnic@yahoo.co.uk>
2022-04-28 18:17:11 +0000

    added matched pairs numbers in javascript

--- assets/game.js
+++ assets/game.js
@@ -2,69 +2,117 @@
 
 const section = document.querySelector('section');
 const paired = document.querySelector('span');
-const same = 0;
+let same = 0;
 
 paired.textContent = same;
 
 // game pictures
-const pictures = () => [
-    {imgsrc: "assets/images/1.jpg", name: "1"},
-    {imgsrc: "assets/images/2.jpg", name: "2"},
-    {imgsrc: "assets/images/chucky.jpg", name: "chucky"},
-    {imgsrc: "assets/images/freddie.jpg", name: "freddie"},
-    {imgsrc: "assets/images/jason.jpg", name: "jason"},
-    {imgsrc: "assets/images/jigsaw.jpg", name: "jigsaw"},
-    {imgsrc: "assets/images/pennywise.jpg", name: "1pennywise"},
-    {imgsrc: "assets/images/scream.jpg", name: "scream"},
-    {imgsrc: "assets/images/1.jpg", name: "1"},
-    {imgsrc: "assets/images/2.jpg", name: "2"},
-    {imgsrc: "assets/images/chucky.jpg", name: "chucky"},
-    {imgsrc: "assets/images/freddie.jpg", name: "freddie"},
-    {imgsrc: "assets/images/jason.jpg", name: "jason"},
-    {imgsrc: "assets/images/jigsaw.jpg", name: "jigsaw"},
-    {imgsrc: "assets/images/pennywise.jpg", name: "1pennywise"},
-    {imgsrc: "assets/images/scream.jpg", name: "scream"},
+
+const pictures = () => [{
+        imgsrc: "assets/images/1.jpg",
+        name: "1"
+    },
+    {
+        imgsrc: "assets/images/2.jpg",
+        name: "2"
+    },
+    {
+        imgsrc: "assets/images/chucky.jpg",
+        name: "chucky"
+    },
+    {
+        imgsrc: "assets/images/freddie.jpg",
+        name: "freddie"
+    },
+    {
+        imgsrc: "assets/images/jason.jpg",
+        name: "jason"
+    },
+    {
+        imgsrc: "assets/images/jigsaw.jpg",
+        name: "jigsaw"
+    },
+    {
+        imgsrc: "assets/images/pennywise.jpg",
+        name: "1pennywise"
+    },
+    {
+        imgsrc: "assets/images/scream.jpg",
+        name: "scream"
+    },
+    {
+        imgsrc: "assets/images/1.jpg",
+        name: "1"
+    },
+    {
+        imgsrc: "assets/images/2.jpg",
+        name: "2"
+    },
+    {
+        imgsrc: "assets/images/chucky.jpg",
+        name: "chucky"
+    },
+    {
+        imgsrc: "assets/images/freddie.jpg",
+        name: "freddie"
+    },
+    {
+        imgsrc: "assets/images/jason.jpg",
+        name: "jason"
+    },
+    {
+        imgsrc: "assets/images/jigsaw.jpg",
+        name: "jigsaw"
+    },
+    {
+        imgsrc: "assets/images/pennywise.jpg",
+        name: "1pennywise"
+    },
+    {
+        imgsrc: "assets/images/scream.jpg",
+        name: "scream"
+    },
 ];
- 
+
 // Random Cards
 const shuffle = () => {
     const slasherData = pictures();
-    slasherData.sort(() => Math.random() -0.5);
+    slasherData.sort(() => Math.random() - 0.5);
     return slasherData;
-   };
- 
- 
- 
+};
+
+
+
 // card data
 
 const cards = () => {
-const slasherData = shuffle ();
-   
-// html data
+    const slasherData = shuffle();
 
-slasherData.forEach((item) => {
-const slasherCard = document.createElement('div');
-const front = document.createElement('img');
-const back = document.createElement('div');
-slasherCard.classList = "slasherCard";
-front.classList = "front";
-back.classList = "back";
+    // html data
 
-// adding pictures onto the cards
+    slasherData.forEach((item) => {
+        const slasherCard = document.createElement('div');
+        const front = document.createElement('img');
+        const back = document.createElement('div');
+        slasherCard.classList = "slasherCard";
+        front.classList = "front";
+        back.classList = "back";
 
-front.src = item.imgsrc;
-slasherCard.setAttribute('name', item.name);
-section.appendChild(slasherCard);
-slasherCard.appendChild(front);
-slasherCard.appendChild(back);
- 
-slasherCard.addEventListener("click", (e) => {
-    slasherCard.classList.toggle("toggleCard");
-    checkCards(e);
- 
-   
-});
-});
+        // adding pictures onto the cards
+
+        front.src = item.imgsrc;
+        slasherCard.setAttribute('name', item.name);
+        section.appendChild(slasherCard);
+        slasherCard.appendChild(front);
+        slasherCard.appendChild(back);
+
+        slasherCard.addEventListener("click", (e) => {
+            slasherCard.classList.toggle("toggleCard");
+            checkCards(e);
+
+
+        });
+    });
 };
 
 // checkCards
@@ -74,29 +122,31 @@ const checkCards = (e) => {
     const clickedCard = e.target;
     clickedCard.classList.add("flipped");
     const flippedCards = document.querySelectorAll('.flipped');
-    
+
     //To check if cards match
 
-if(flippedCards.length === 2) {
-   if (
-   flippedCards[0].getAttribute('name') ===
-   flippedCards[1].getAttribute('name')
-   ) {
-       console.log("correct");
-       flippedCards.forEach((slasherCard) => {
-           slasherCard.classList.remove("flipped");
-           slasherCard.style.pointerEvents = "none";
-       });
-   } else {
-       console.log ("try again");
-       flippedCards.forEach((slasherCard) => {
-           slasherCard.classList.remove('flipped');
-           setTimeout(() => slasherCard.classList.remove("toggleCard"), 1000);
-       });
+    if (flippedCards.length === 2) {
+        if (
+            flippedCards[0].getAttribute('name') ===
+            flippedCards[1].getAttribute('name')
+        ) {
+            console.log("correct");
+            flippedCards.forEach((slasherCard) => {
+                slasherCard.classList.remove("flipped");
+                slasherCard.style.pointerEvents = "none";
 
-   }
-};
+            });
+            same++;
+            paired.textContent = same;
+        } else {
+            console.log("try again");
+            flippedCards.forEach((slasherCard) => {
+                slasherCard.classList.remove('flipped');
+                setTimeout(() => slasherCard.classList.remove("toggleCard"), 1000);
+            });
+
+        }
+    };
 };
 
 cards();
-
